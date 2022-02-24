413d8014aa57a77dbcbe07f5a810540c2dfbc96a
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-02-24 10:23:49 +0800

    feat: authority management

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -30,121 +30,133 @@ import Layout from '@/layout'
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
+
+// 常量路由：任何用户和角色都可以看见
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
 
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
 
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '仪表盘', icon: 'dashboard' }
-    }]
-  },
-  {
-    path: '/acl',
-    name: 'Acl',
-    component: Layout,
-    meta: { title: '权限管理', icon: 'el-icon-lock'},
+    redirect: "/dashboard",
     children: [
       {
-        name: 'User',
-        path: 'user/list',
-        component: () => import('@/views/acl/user/list'),
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "仪表盘", icon: "dashboard" },
+      },
+    ],
+  },
+];
+
+// 异步路由： 不同权限的角色用户才能看见
+export const asyncRoutes = [
+  {
+    path: "/acl",
+    name: "Acl",
+    component: Layout,
+    meta: { title: "权限管理", icon: "el-icon-lock" },
+    children: [
+      {
+        name: "User",
+        path: "user/list",
+        component: () => import("@/views/acl/user/list"),
         meta: {
-          title: '用户管理'
+          title: "用户管理",
         },
       },
       {
-        name: 'Role',
-        path: 'role/list',
-        component: () => import('@/views/acl/role/list'),
+        name: "Role",
+        path: "role/list",
+        component: () => import("@/views/acl/role/list"),
         meta: {
-          title: '角色管理',
+          title: "角色管理",
         },
       },
       {
-        name: 'RoleAuth',
-        path: 'role/auth:id',
-        component: () => import('@/views/acl/role/roleAuth'),
+        name: "RoleAuth",
+        path: "role/auth:id",
+        component: () => import("@/views/acl/role/roleAuth"),
         meta: {
-          title: '角色授权',
-          activeMenu: '/acl/role/list'
+          title: "角色授权",
+          activeMenu: "/acl/role/list",
         },
         hidden: true,
       },
       {
-        path: 'permission/list',
-        name: 'Permission',
-        component: () => import('@/views/acl/permission/list'),
-        meta: { title: '菜单管理' },
+        path: "permission/list",
+        name: "Permission",
+        component: () => import("@/views/acl/permission/list"),
+        meta: { title: "菜单管理" },
       },
-      
-    ]
+    ],
   },
   {
-    path: '/product',
-    name: 'Product',
+    path: "/product",
+    name: "Product",
     component: Layout,
-    meta: { title: '商品管理', icon: 'el-icon-goods' },
+    meta: { title: "商品管理", icon: "el-icon-goods" },
     children: [
       {
-        path: 'trademark',
-        name: 'TradeMark',
-        component: () => import('@/views/product/TradeMark'),
-        meta: { title: '品牌管理' }
+        path: "trademark",
+        name: "TradeMark",
+        component: () => import("@/views/product/TradeMark"),
+        meta: { title: "品牌管理" },
       },
       {
-        path: 'attr',
-        name: 'Attr',
-        component: () => import('@/views/product/Attr'),
-        meta: { title: '平台属性管理' }
+        path: "attr",
+        name: "Attr",
+        component: () => import("@/views/product/Attr"),
+        meta: { title: "平台属性管理" },
       },
       {
-        path: 'spu',
-        name: 'Spu',
-        component: () => import('@/views/product/Spu'),
-        meta: { title: 'Spu管理' }
+        path: "spu",
+        name: "Spu",
+        component: () => import("@/views/product/Spu"),
+        meta: { title: "Spu管理" },
       },
       {
-        path: 'sku',
-        name: 'Sku',
-        component: () => import('@/views/product/Sku'),
-        meta: { title: 'Sku管理' }
-      }
-    ]
+        path: "sku",
+        name: "Sku",
+        component: () => import("@/views/product/Sku"),
+        meta: { title: "Sku管理" },
+      },
+    ],
   },
+];
 
-
+// 任意路由： 当路径出现错误的时候重定向404
+export const anyRoutes = {
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  path: "*",
+  redirect: "/404",
+  hidden: true,
+};
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,128 @@
-import { login, logout, getInfo } from '@/api/user'
-import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
+import { login, logout, getInfo } from "@/api/user";
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import router, { asyncRoutes, resetRouter, anyRoutes, constantRoutes } from "@/router";
 
 const getDefaultState = () => {
   return {
     token: getToken(),
-    name: '',
-    avatar: ''
-  }
-}
+    name: "",
+    avatar: "",
+    // 菜单信息
+    routes: [],
+    // 角色信息
+    roles: [],
+    // 按钮权限
+    buttons: [],
+    // 项目配置的异步路由和服务器根据不同角色返回的路由对比后计算的最终需要的异步路由
+    resultAsyncRoutes: [],
+    // 最终需要的所有路由
+    resultAllRoutes: []
+  };
+};
 
-const state = getDefaultState()
+const state = getDefaultState();
 
 const mutations = {
   RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+    Object.assign(state, getDefaultState());
   },
   SET_TOKEN: (state, token) => {
-    state.token = token
+    state.token = token;
   },
-  SET_NAME: (state, name) => {
-    state.name = name
+  // 存储用户信息
+  SET_USERINFO: (state, userInfo) => {
+    state.name = userInfo.name;
+    state.avatar = userInfo.avatar;
+    state.routes = userInfo.routes;
+    state.buttons = userInfo.buttons;
+    state.roles = userInfo.roles;
   },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+  // 最终需要的所有路由
+  SET_RESULTASYNCROUTES: (state, asyncRoutes) => {
+    state.resultAsyncRoutes = asyncRoutes;
+    state.resultAllRoutes = constantRoutes.concat(state.resultAsyncRoutes, anyRoutes)
+    router.addRoutes(state.resultAllRoutes)
   }
-}
+};
 
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo
+    const { username, password } = userInfo;
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      login({ username: username.trim(), password: password })
+        .then((response) => {
+          const { data } = response;
+          commit("SET_TOKEN", data.token);
+          setToken(data.token);
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   },
 
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      getInfo(state.token)
+        .then((response) => {
+          //获取用户信息:返回数据包含：用户名name、用户头像avatar、routes[返回的标志:不同的用户应该展示哪些菜单的标记]、roles（用户角色信息）、buttons【按钮的信息：按钮权限用的标记】
+          const { data } = response;
+          //vuex存储用户全部的信息
+          commit("SET_USERINFO", data);
+          commit('SET_RESULTASYNCROUTES', computedAsyncRoutes(asyncRoutes, data.routes))
+          resolve(data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   },
 
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      logout(state.token)
+        .then(() => {
+          removeToken(); // must remove  token  first
+          resetRouter();
+          commit("RESET_STATE");
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   },
 
   // remove token
   resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
-  }
+    return new Promise((resolve) => {
+      removeToken(); // must remove  token  first
+      commit("RESET_STATE");
+      resolve();
+    });
+  },
+};
+
+const computedAsyncRoutes = (asyncRoutes, routes) => {
+  // 过滤当前用户需要展示的异步路由
+  return asyncRoutes.filter(item => {
+    if (routes.indexOf(item.name) != -1) {
+      // 递归处理多级路由的情况
+      if (item.children && item.children.length) {
+        item.children = computedAsyncRoutes(item.children, routes)
+      }
+      return true
+    }
+  })
 }
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions
-}
-
+  actions,
+};
